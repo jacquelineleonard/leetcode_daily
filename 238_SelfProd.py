5314b6a482cafe20cd1ce5ea50c667dd83cc5c41
neema-s <shrivastavaneema@gmail.com>
2026-03-30 23:39:26 +0530

    238. Product of Array Except Self

---
+++ 238_SelfProd.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums)
+        res = [1] * n
+        
+        prefix = 1
+        for i in range(n):
+            res[i] = prefix
+            prefix *= nums[i]
+        
+        suffix = 1
+        for i in range(n-1, -1, -1):
+            res[i] *= suffix
+            suffix *= nums[i]
+        
+        return res
+    
+#     Time: O(n)
+# Space: O(1)
